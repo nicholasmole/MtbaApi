b68e4626b949740a1d57faee3cfeda23eb8f9321
Nick Mole <nmole065@g.rwu.edu>
2019-01-25 08:58:33 -0500

    [feature] Update comment documentation
    Update comments in module to explain
    each function in more detail

--- src/Controller/MtbaApiController.php
+++ src/Controller/MtbaApiController.php
@@ -18,6 +18,11 @@ use \NicholasMole\JsonApiRequest\JsonApiRequest;
 class MtbaApiController {
 
   /**
+   * @param object $request - Api Request Response data
+   */
+  private $request;
+  
+  /**
    * Sort array by attributes type
    *
    * @param array $a - first array to reorder
@@ -30,22 +35,52 @@ class MtbaApiController {
   }
 
   /**
+   * Set the MtbaApiController->request param to the json api request data
+   */
+  private function setRequest() {
+    $this->request  = new JsonApiRequest('https://api-v3.mbta.com/routes?filter[type]=0,1,2');
+  }
+
+  /**
+   * Decode the api request
+   * 
+   * @param object $request - Api Request Response data
+   * @return array Json on api data
+   */
+  private function getDecodedResponse($request): array {
+
+    return json_decode($request->getResult(), true);
+
+  }
+
+  private function getData(): array {
+
+    // Set Request Data
+    $this->setRequest();
+
+    // Decode Request
+    $data =  $this->getDecodedResponse($this->request);
+
+    // Sort Request Data
+    usort($data[data], array($this, $this->sort_by_attributes_type));
+
+    // Return the data
+    return $data[data];
+
+  }
+
+  /**
    * Display template for mtba_index view
    * 
    * @return array display twig theme
    */
   public function content() {
 
-    $request = new JsonApiRequest('https://api-v3.mbta.com/routes?filter[type]=0,1,2');
-
-    $data = json_decode($request->getResult(), true);
-
-    usort($data[data], array($this, $this->sort_by_attributes_type));
-
     return array(
       '#theme' => 'mtba_index',
-      '#route_data' => $data[data]
+      '#route_data' => $this->getData()
     );
+
   }
 
 
--- src/Controller/MtbaApiTrainRoutesController.php
+++ src/Controller/MtbaApiTrainRoutesController.php
@@ -19,6 +19,87 @@ use \NicholasMole\JsonApiRequest\JsonApiRequest;
 class MtbaApiTrainRoutesController {
 
   /**
+   * @param object $request - Api Request Response data
+   */
+  private $request;
+
+  /**
+   * Set the MtbaApiController->request param to the json api request data
+   */
+  private function setRequest($trainroutes) {
+
+    $this->request = new JsonApiRequest("https://api-v3.mbta.com/schedules?page[limit]=12&include=stop&filter[min_time]=".date("H:i")."&filter[route]=$trainroutes");
+
+  }
+
+  /**
+   * Decode the api request
+   * 
+   * @param object $request - Api Request Response data
+   * @return array Json on api data
+   */
+  private function getDecodedResponse($request): array {
+
+    return json_decode($request->getResult(), true);
+
+  }
+
+  /**
+   * Set Time Zone for php file
+   */
+  private function setTimeZone() {
+
+    date_default_timezone_set('America/New_York');
+
+  }
+
+  /**
+   * Get Attributes Arrival Time from array data
+   * 
+   * @param array $data current data array from route data
+   */
+  private function getAttributesArrivalTime($data) {
+
+    return $data[attributes][arrival_time];
+
+  }
+
+  /**
+   * Get Attributes Name from array data
+   * 
+   * @param array $data current data array from route data
+   */
+  private function getAttributesName($data) {
+
+    return $data[attributes][name];
+
+  }
+
+  /**
+   * Get Relationships Stop Data ID from array data
+   * 
+   * @param array $data current data array from route data
+   */
+  private function getRelationshipsStopDataId($data) {
+
+    return $data[relationships][stop][data][id];
+
+  }
+
+  /**
+   * Get the hour:minute time stamp from arrival_time
+   * 
+   * @param string $arrival_time current data arrival time data
+   */
+  private function getArrivalTimeHourAndMinutes($arrival_time) {
+
+    return date('h:i', strtotime($arrival_time));
+
+  }
+
+
+
+  /**
    * Generate Train Schedule table
    *
    * Turns api schedule data into 
@@ -28,7 +109,7 @@ class MtbaApiTrainRoutesController {
    * 
    * @return array $new_table - new table contains data to construct Routes Schedule page
    */
-  private static function generate_schedule_table($schedule_data, $included_data): array
+  private function generate_schedule_table($schedule_data, $included_data): array
   {
 
     $new_table = [];
@@ -36,22 +117,53 @@ class MtbaApiTrainRoutesController {
     $collect_names = [];
 
     for ($i = 0; $i < count($included_data); $i++) {
-      $collect_names[$included_data[$i][id]] = $included_data[$i][attributes][name];
+
+      // Get names from request data
+      $collect_names[$included_data[$i][id]] = $this->getAttributesName($included_data[$i]);
+
     }
 
     for ($i = 0; $i < count($schedule_data); $i++) {
-      $id = $schedule_data[$i][relationships][stop][data][id];
 
-      $arrival_time_unconverted = $schedule_data[$i][attributes][arrival_time];
+      // get relationship stop id data
+      $id = $this->getRelationshipsStopDataId($schedule_data[$i]);
 
-      $arrival_time = date('h:i', strtotime($arrival_time_unconverted));
+      // get attributes arrival_time
+      $arrival_time_unconverted = $this->getAttributesArrivalTime(schedule_data[$i]);
 
+      // convert arrival time to hour:minute
+      $arrival_time = $this->getArrivalTimeHourAndMinutes($arrival_time_unconverted);
+
+      //create new array for twig templating
       array_push($new_table, [
         "title"=>$collect_names[$id],
         "arrival_time"=>$arrival_time
       ]);
+
     }
+
     return $new_table;
+  }
+
+  /**
+   * Contruct the route data into a table to be used by twig
+   * 
+   * @param array $schedule_data - Json API schedule data
+   */
+  private function getRouteDatatoConstructScheduleTable($trainroutes) {
+
+    // set time zone for php
+    $this->setTimeZone();
+
+    //Set request for class variable
+    $this->setRequest($trainroutes);
+
+    // schedule data for table display
+    $schedule_data =  $this->getDecodedResponse($this->request);
+
+    // return table for twig templating
+    return  $this->generate_schedule_table($schedule_data[data], $schedule_data[included]);
+
   }
 
   /**
@@ -63,18 +175,11 @@ class MtbaApiTrainRoutesController {
    */
   public function content($trainroutes = NULL) {
 
-    date_default_timezone_set('America/New_York');
-
-    $schedule = new JsonApiRequest("https://api-v3.mbta.com/schedules?page[limit]=12&include=stop&filter[min_time]=".date("H:i")."&filter[route]=$trainroutes");
-
-    $schedule_data = json_decode($schedule->getResult(), true);
-
-    $constructTable = $this->generate_schedule_table($schedule_data[data], $schedule_data[included]);
-
     return array(
       '#theme' => 'mtba_list',
-      '#route_data' => $constructTable,
-      '#route_title' => $trainroutes
+      '#route_data' => $this->getRouteDatatoConstructScheduleTable($trainroutes),
+      '#route_title' => $trainroutes 
     );
   }
+
 }
